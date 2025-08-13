83a5846b643d11ba52d3a4b833af28fe1823035b
Valarie A. Paiz <paiz.valerie27@gmail.com>
2025-08-13 11:00:50 -0500

    New update

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,32 @@
-const initalCards = [
+const initialCards = [
   {
    name: "Val Thorens",
    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
   },
 
 {
-name: "Restaurant terrace",
-link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+  name: "Restaurant terrace",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
 },
 
 {
   name: "An outdoor cafe",
-link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
 },
 
 {
   name: "A very long bridge, over the forest and through the trees",
-link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
 },
 
 {
-name: "Tunnel with morning light",
-link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+ name: "Tunnel with morning light",
+ link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
 },
 
 {
   name: "Mountain house",
-link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
 },
 ];
 
@@ -55,7 +55,7 @@ newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
 
-initalCards.forEach(function (card) {
+initialCards.forEach(function (card) {
   console.log(card.name);
   console.log(card.link);
 });
